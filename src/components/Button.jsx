01c28e9f7e36188d8e65af13445bc59039346891
Renato Freitas <renattomoreyra@hotmail.com>
2022-11-29 21:02:20 -0300

    Business, Button Components
    Finished the Business, and the Button Components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import styles from "../styles";
 
-function Button() {
+function Button({ styles, text }) {
   return (
-    <div>Button</div>
+    <button type='button' 
+    className={`py-4 px-6
+    bg-blue-gradient font-poppins font-medium
+    text-[18px] text-primary outline-none
+    ${styles}`}>
+      {text}
+    </button>
   )
 }
 
